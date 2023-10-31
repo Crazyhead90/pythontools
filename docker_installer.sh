8b5775d410e4b17f0aae8e15022ba56d350c3ee4
Crazyhead90 <Dannydewit@gmail.com>
2023-10-31 16:50:14 +0100

    Update docker_installer.sh

--- docker_installer.sh
+++ docker_installer.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 # Add Docker's official GPG key:
-if os.path.isfile('/usr/share/keyrings/docker.gpg'):
-  os.remove('/usr/share/keyrings/docker.gpg')
+[ -f '/usr/share/keyrings/docker.gpg' ] && rm '/usr/share/keyrings/docker.gpg'
 sudo apt-get update
 sudo apt-get install ca-certificates curl gnupg
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker.gpg
 sudo chmod a+r /usr/share/keyrings/docker.gpg
 
 # Add the repository to Apt sources:
-if os.path.isfile('/etc/apt/sources.list.d/docker.list'):
-  os.remove('/etc/apt/sources.list.d/docker.list')
+[ -f '/etc/apt/sources.list.d/docker.list' ] && rm '/etc/apt/sources.list.d/docker.list'
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
