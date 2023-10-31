4615d8d2a2582932aa29deaf9674338f2c2dd5f4
Crazyhead90 <Dannydewit@gmail.com>
2023-10-31 11:43:47 +0100

    Update tmp_msfinstall_workaround.py

--- tmp_msfinstall_workaround.py
+++ tmp_msfinstall_workaround.py
@@ -5,10 +5,10 @@ with open('msfinstall') as f:
   msfinstall_lines = f.readlines()
 
 for line in range(len(msfinstall_lines)):
-  if msfinstall_lines[line] == '  print_pgp_key | apt-key add -':
-    msfinstall_lines[line] = '  print_pgp_key | sudo gpg --dearmor -o /usr/share/keyrings/metasploit-framework.gpg'
-  if msfinstall_lines[line] == '  echo "deb $DOWNLOAD_URI/apt lucid main" > $LIST_FILE':
-    msfinstall_lines[line] = '  echo "deb [signed-by=/usr/share/keyrings/metasploit-framework.gpg] $DOWNLOAD_URI/apt lucid main" > $LIST_FILE'
+  if msfinstall_lines[line] == '  print_pgp_key | apt-key add -\n':
+    msfinstall_lines[line] = '  print_pgp_key | sudo gpg --dearmor -o /usr/share/keyrings/metasploit-framework.gpg\n'
+  if msfinstall_lines[line] == '  echo "deb $DOWNLOAD_URI/apt lucid main" > $LIST_FILE\n':
+    msfinstall_lines[line] = '  echo "deb [signed-by=/usr/share/keyrings/metasploit-framework.gpg] $DOWNLOAD_URI/apt lucid main" > $LIST_FILE\n'
 os.remove('msfinstall')
 with open('msfinstall','w') as f:
   for item in msfinstall_lines:
