9828b18fba595df4fa184201fb19cda0590cf934
Crazyhead90 <Dannydewit@gmail.com>
2023-10-31 16:12:26 +0100

    Update bloodhound_installer.sh

--- bloodhound_installer.sh
+++ bloodhound_installer.sh
@@ -1,34 +1,10 @@
 #!/bin/bash
 
-#Install Open JDK 11:
-sudo apt-get install openjdk-11-jdk
+#Download latest Bloodhound version
+curl -s https://api.github.com/repos/BloodHoundAD/BloodHound/releases/latest | grep 'browser_download_url.*linux-x64.zip' | cut -d : -f 2,3 | tr -d \" | wget -qi -
 
-#Add the neo4j repo to your apt sources:
-[ ! -f "/usr/share/keyrings/neotechnology.gpg"] && curl -fsSL https://debian.neo4j.com/neotechnology.gpg.key | sudo gpg --dearmor -o /usr/share/keyrings/neotechnology.gpg && echo 'deb [signed-by=/usr/share/keyrings/neotechnology.gpg] https://debian.neo4j.com stable 4.4' > /etc/apt/sources.list.d/neo4j.list
+#Unzip
+unzip BloodHound-linux-x64.zip
 
-#Install apt-transport-https with apt
-apt-get update
-
-#Install neo4j community edition using apt:
-sudo apt-get install neo4j
-
-#Stop neo4j
-sudo systemctl stop neo4j
-
-#Start neo4j as a console application and verify it starts up without errors:
-cd /usr/bin
-sudo ./neo4j console
-
-#Run systemctl to start neo4j
-sudo systemctl start neo4j
-
-#install Node.js
-[ ! -f "/usr/share/keyrings/neotechnology.gpg"]
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /usr/share/keyrings/nodesource.gpg
-NODE_MAJOR=21
-echo "deb [signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-sudo apt-get update
-sudo apt-get install nodejs -y
-
-#Install electron-packager
-sudo npm install -g electron-packager
+#Remove zip
+rm Bloodhound-linux-x64.zip
