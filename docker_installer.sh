537e4187dff240b31edb4e9b6c9d8a84333ffdc2
Crazyhead90 <Dannydewit@gmail.com>
2023-10-31 18:05:13 +0100

    Update docker_installer.sh

--- docker_installer.sh
+++ docker_installer.sh
@@ -19,5 +19,5 @@ sudo apt-get update
 sudo apt-get install -y docker.io docker-compose-plugin
 
 wget https://raw.githubusercontent.com/SpecterOps/bloodhound/main/examples/docker-compose/docker-compose.yml
-echo "docker compose -f - up" > bloodhound_start.sh
-echo "docker compose down" > bloodhound_delete_data.sh
+echo "#!/bin/bash\ndocker compose -f - up" > bloodhound_start.sh
+echo "#!/bin/bash\ndocker compose down" > bloodhound_delete_data.sh
